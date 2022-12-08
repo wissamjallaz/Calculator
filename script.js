4bf7da12c0f3d2a9cfb9bc67e4f672afc9fed592
Wissam Allaz <wissam.j.allaz@gmail.com>
2022-12-08 11:28:55 +0200

    added operator functions and display screen

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+display = document.querySelector('.display')
+numberButtons = document.querySelectorAll('.number');
+operatorButtons = document.querySelectorAll('.operators')
 
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function percentage(a){
+    a/100
+}
+
+function operate(operator, a, b){
+    switch (operator){
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;
+        case "%":
+            percentage(a,b);
+            break;
+        default:
+            console.log("Enter Valid Operator");
+    }
+}
+
+function populate(){
+    numberButtons.forEach(button=>{
+        button.addEventListener('click', ()=>{
+            display.textContent = +button.innerHTML;
+            input = +button.innerHTML;
+    })
+})}
+
+operatorButtons.forEach(operator=>{
+    operator.addEventListener('click', ()=>{
+
+    })
+})
